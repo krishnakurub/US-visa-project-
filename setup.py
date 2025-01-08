c47edeb514ec6565061deaad4f4dfb55c239fe68
Your Name <you@example.com>
2025-01-08 03:44:03 -0800

    updated

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="krishna"
+    author="krishna",
     author_email="mohankrishna910@gmail.com",
     packages=find_packages())  ### it look for constructor in evry folder (ie folders inside us_visa so tat we directly do" from us_visa import  some thing",,,us _visa becomes local pakcage)
