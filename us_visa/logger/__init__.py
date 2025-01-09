e2b3ec5bd79e6c3de0f38f51966ce02f3210787b
Your Name <you@example.com>
2025-01-09 05:41:32 -0800

    utils added

---
+++ us_visa/logger/__init__.py
@@ -0,0 +1,24 @@
+
+#### python logging website
+
+import logging
+import os
+
+from from_root import from_root
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"  #logging string
+
+log_dir='logs'  # creating log folder
+
+logs_path = os.path.join(from_root(),log_dir,LOG_FILE) ## creating file
+
+os.makedirs(log_dir,exist_ok=True)  ## creating a folder
+
+## logging the information below code
+logging.basicConfig(
+    filename=logs_path,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+
+) 
